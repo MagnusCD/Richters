7eacc9bfb4467eb8171de2cb92d81b21db1e76f4
MagnusCD <magnus.danielsen@hotmail.com>
2024-01-31 12:25:29 +0100

    update

--- static/js/script.js
+++ static/js/script.js
@@ -32,6 +32,14 @@ window.addEventListener("scroll", reveal);
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
 
 
 
@@ -70,12 +78,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+
 
 
 
