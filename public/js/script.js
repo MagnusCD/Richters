b8bdf6a1b6dd322aadd87c1d7ceaf68d5614ee0f
Magnus Danielsen <magnus.danielsen@ffw.com>
2024-02-25 17:10:40 +0100

    update

--- public/js/script.js
+++ public/js/script.js
@@ -43,9 +43,9 @@ document.addEventListener('DOMContentLoaded', function() {
   function updateFacebookBoxWidth() {
       var fbBox = document.querySelector('.fb-like-box');
       if (window.innerWidth > 1300) {
-          fbBox.setAttribute('data-width', '600');
+          fbBox.setAttribute('data-width', '600px');
       } else {
-          fbBox.setAttribute('data-width', '300');
+          fbBox.setAttribute('data-width', '300px');
       }
   }
 
