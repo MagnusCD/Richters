82cdbf04d1da4a5d613f6fa1afdf152cec6efdca
Magnus Danielsen <magnus.danielsen@ffw.com>
2024-02-25 14:11:26 +0100

    update

--- public/js/purecookie.js
+++ public/js/purecookie.js
@@ -1,6 +1,6 @@
 // --- Config --- //
 var purecookieTitle = "Cookies."; // Title
-var purecookieDesc = "By using this website, you automatically accept that we use cookies."; // Description
+var purecookieDesc = "Ved at bruge denne hjemmeside, accepterer du samtidigt, at vi gør brug af cookies."; // Description
 var purecookieLink = '<a href="https://www.cssscript.com/privacy-policy/" target="_blank">What for?</a>'; // Cookiepolicy link
 var purecookieButton = "Understood"; // Button text
 // ---        --- //
