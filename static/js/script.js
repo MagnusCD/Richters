1f56eddbc5f88cf60c7d65e46c41aab8728bf4ce
MagnusCD <magnus.danielsen@hotmail.com>
2024-02-26 02:05:27 +0100

    update

--- static/js/script.js
+++ static/js/script.js
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let scrollPosition = window.scrollY;
 
     // Apply a downward movement to the hero image
-    topnavImage.style.transform = `translateY(${scrollPosition * 0.17}px)`;
+    topnavImage.style.transform = `translateY(${scrollPosition * 0.15}px)`;
   }
 
   // Add a scroll event listener to trigger the handleScroll function
