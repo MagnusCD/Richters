843df19870946f6e7df8d1b0e11c7fa9ac1f44c1
Magnus Danielsen <magnus.danielsen@ffw.com>
2024-02-29 13:42:40 +0100

    update

--- static/js/script.js
+++ static/js/script.js
@@ -101,6 +101,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var modalImg = document.getElementById("img01");
+
+// Get all images with the class "gallery-images" and attach the click event to them
+var images = document.getElementsByClassName('gallery-images');
+var captionText = document.getElementById("caption");
+for (var i = 0; i < images.length; i++) {
+    images[i].onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        modalImg.alt = this.alt;
+        captionText.innerHTML = this.alt; // If you want to use the alt attribute as a caption
+    }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+
+
 
 
 
