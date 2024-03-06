2fb9ed409c38e36ce9f18a0709d896e932f78794
Magnus Danielsen <magnus.danielsen@ffw.com>
2024-03-06 19:22:43 +0100

    update

--- static/js/script.js
+++ static/js/script.js
@@ -84,6 +84,35 @@ var purecookieLink = '<a href="https://www.cssscript.com/privacy-policy/" target
 var purecookieButton = "Forstået"; // Button text
 // ---        --- //
 
+
+function pureFadeIn(elem, display){
+  var el = document.getElementById(elem);
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .02) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+};
+function pureFadeOut(elem){
+  var el = document.getElementById(elem);
+  el.style.opacity = 1;
+
+  (function fade() {
+    if ((el.style.opacity -= .02) < 0) {
+      el.style.display = "none";
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+};
+
+
+
 function cookieConsent() {
   if (!getCookie('purecookieDismiss')) {
     // Create the container div
@@ -116,6 +145,9 @@ function cookieConsent() {
     // Append the container to the body
     document.body.appendChild(consentContainer);
 
+    // Fade in effect
+    pureFadeIn("cookieConsentContainer");
+
     // Attach event listener to the button instead of using onClick
     buttonLink.addEventListener('click', function() {
       purecookieDismiss();
@@ -128,19 +160,10 @@ function cookieConsent() {
 
 function purecookieDismiss() {
   setCookie('purecookieDismiss','1',7);
+  pureFadeOut("cookieConsentContainer");
 }
 
 window.onload = function() { cookieConsent(); };
-
-
-
-
-
-
-
-
-
-
 
 
 
